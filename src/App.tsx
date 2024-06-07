a3eaa356d74efedb6e797ac12fa57b63765c7a7b
Lee Martin <lee.martin@quotech.io>
2024-06-07 11:49:58 +0100

    feat: Add mantine for example

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@mantine/core/styles.css';
+
+import { Button, createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <MantineProvider theme={theme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +25,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,7 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </MantineProvider>
   )
 }
 
